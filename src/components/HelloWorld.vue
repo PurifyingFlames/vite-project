17e07e11bfcc40f14e75f40bff69afc370370cbe
Tao <tao@TaodeMacBook-Pro.local>
2022-05-20 19:37:18 +0800

    注释

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -116,7 +116,7 @@ onMounted(() => {
 
   // console.log(store.state.count);
 
-  console.log('%c [  ]-119', 'font-size:13px; background:pink; color:#bf2c9f;', useMapState(['count']))
+  // console.log('%c [  ]-119', 'font-size:13px; background:pink; color:#bf2c9f;', useMapState(['count']))
   n.value = 6;
   nextTick(() => {
     // console.log(ref1.value.innerHTML);
